7e1af535061187c8c87fa31ecd6f756793227bb0
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-06-05 23:40:46 +0900

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import { App } from './app';
+import { UIProvider } from '@yamada-ui/react';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const container = document.getElementById('app');
+
+const root = createRoot(container!);
+
 root.render(
-  <React.StrictMode>
+  <UIProvider>
     <App />
-  </React.StrictMode>
+  </UIProvider>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
